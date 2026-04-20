eba4d67c189afcee03ae3f0b007731544d4cbe7a
LSH <tkdgh1135@naver.com>
2026-04-20 22:24:54 +0900

    initial setting

---
+++ backend/src/main/java/com/musinsaclone/MusinsacloneApplication.java
@@ -0,0 +1,12 @@
+package com.musinsaclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsacloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsacloneApplication.class, args);
+    }
+}
---
+++ frontend/src/api/client.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+const client = axios.create({
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8080/api',
+  withCredentials: true,
+})
+
+client.interceptors.response.use(
+  (res) => res,
+  (err) => Promise.reject(err),
+)
+
+export default client
